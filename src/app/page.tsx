805ec5702586b2be0a1fd7aa27fea2b27571b3dd
prakit srakaew <prakitsrams@gmail.com>
2025-05-28 17:05:48 +0700

    feat: add task tracker without delete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { BirthdayCal } from "@/components/birthday-calculator";
+import { TaskTracker } from "@/components/task-tracker";
 import { Toaster } from "sonner";
 
 export default function Home() {
   return (
     <div>
-      <BirthdayCal />
+      <div className="grid grid-cols-2">
+        <TaskTracker />
+        <BirthdayCal />
+      </div>
+
       <div>
         <Toaster />
       </div>
